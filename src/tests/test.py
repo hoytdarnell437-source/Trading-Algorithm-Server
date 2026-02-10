488787c566f476daed245f07ab21d03839c57dca
hoytdarnell437-source <hoytdarnell437@gmail.com>
2026-02-10 12:39:10 -0500

    pandas

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,13 +1,17 @@
 import yfinance as yf
+import pandas as pd
 from MACD import macd
 
 google = yf.Ticker("GOOG")
+data = google.history()
+df = pd.DataFrame(data)
+closePrices = df["Close"].tolist()
 
-prices = [10, 11, 12, 13, 14, 15, 14, 13, 12, 11, 10, 9, 8, 9, 10, 11, 12, 13, 14, 15]
 previousResults = 0
 previousSignal = 0
 
-for price in prices:
+for price in closePrices:
+    # IMPORTANT action is only valid after the first ~30 iterations
     previousResults, previousSignal, action = macd(price, previousResults, previousSignal)
     print("\nResults: ", previousResults, "\nSignal: ", previousSignal, "\nAction: ", action)
 
